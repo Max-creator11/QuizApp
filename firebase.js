40c574b58e317717a970c3c706b94b3b0109abc1
Max-creator11 <78356505+Max-creator11@users.noreply.github.com>
2022-04-13 21:08:50 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 import {getFirestore, setDoc, addDoc, doc} from 'firebase/firestore'
 const app = firebase.initializeApp ({
-        apiKey: "AIzaSyBxZQx-WDLkqxkyYJ5puXk0Ja5jZeIUgKg",
-        authDomain: "quiz-app-b7e0f.firebaseapp.com",
-        projectId: "quiz-app-b7e0f",
-        storageBucket: "quiz-app-b7e0f.appspot.com",
-        messagingSenderId: "895008378936",
-        appId: "1:895008378936:web:e3f889e185eca0679196cd"
+        apiKey: "---",
+        authDomain: "---",
+        projectId: "---",
+        storageBucket: "---",
+        messagingSenderId: "---",
+        appId: "---"
     })
 
 export const auth = app.auth()
